6a6fa2a99b017bf77f6d9097431d77b2d7d26928
Wren Miles <wrendmiles@gmail.com>
2026-01-24 19:42:46 -0800

    Add styling CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import PageNotFound from './components/PageNotFound';
+
 
 function App() {
-  
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
