c1d794d709546252348af0b93b55ef7a3c70a23e
Wren Miles <wrendmiles@gmail.com>
2026-01-30 13:37:24 -0800

    Add typing to resolve warnings

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,18 @@
-const Home = () => {
+import PostFeed from "./PostFeed";
+import type { PostType } from '../types/index';
+
+type HomeProps = {
+  posts: PostType[];
+  isLoading: boolean;
+  fetchError: string;
+};
+
+const Home = ({ posts, isLoading, fetchError }: HomeProps) => {
+  if (isLoading) return <p>Loading posts...</p>;
+  if (fetchError) return <p>Error: {fetchError}</p>;
   return (
     <div className="Home">
-      <h1>Hello World :3</h1>
+      <PostFeed posts={posts} />
     </div>
   );
 };
