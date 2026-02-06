abd450bf5e3f9bc788320a1acf2aa98b880e961d
Wren Miles <wrendmiles@gmail.com>
2026-02-06 13:23:29 -0800

    Add routing in App.tsx for single post page based on the id param

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,27 @@ import Photography from './components/Photography';
 import Poetry from './components/Poetry';
 import Essays from './components/Essays';
 import Fashion from './components/Fashion';
-
 import Home from './components/Home';
+import PostPage from './components/PostPage';
 import PageNotFound from './components/PageNotFound';
 import Layout from './components/Layout';
 
 
 
 function App() {
-
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home  />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="post">
+          <Route path=":id" element={<PostPage />} />
+        </Route>
         <Route path="paintings" element={<Paintings />} />
         <Route path="photography" element={<Photography />} />
         <Route path="poetry" element={<Poetry />} />
         <Route path="essays" element={<Essays />} />
         <Route path="fashion" element={<Fashion />} />
+        <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
   );
