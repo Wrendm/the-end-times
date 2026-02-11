52b4372b34a17f0fd841674f63876f16290b7883
Wren Miles <wrendmiles@gmail.com>
2026-02-11 13:07:33 -0800

    Split app.js out into server.js and app.js

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get(['/', '/index', '/index.html'], (req, res) => {
+    res.send('API is running...');
+});
+
+app.use((req, res) => {
+    res.status(404).json({ message: `Route ${req.originalUrl} not found`});
+});
+
+module.exports = app;
