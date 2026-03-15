62cca8cc0c0cb5037609361cd0ff293c5098e0f1
Wren Miles <wrendmiles@gmail.com>
2026-03-15 14:03:24 -0700

    feat(auth): create `ProtectedRoute` to guard routes based on user authentication

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import type { ReactNode } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../context/authcontext";
+
+type ProtectedRouteProps = {
+  children: ReactNode;
+};
+
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const auth = useContext(AuthContext);
+
+  if (!auth) throw new Error("AuthContext not found");
+
+  if (!auth.user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <>{children}</>;
+}
