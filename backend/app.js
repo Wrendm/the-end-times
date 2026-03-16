bd75727d0bef78b1fa0cf007f631050a39ef4ab4
Wren Miles <wrendmiles@gmail.com>
2026-03-15 17:21:54 -0700

    feat(auth): register auth routes in app

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const errorHandler = require('./middleware/errorHandler')
 const corsOptions = require('./middleware/cors')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
 app.use(corsOptions)
 app.use(express.json())
+app.use(cookieParser())
 
 app.get(['/', '/index', '/index.html'], (req, res) => {
     res.send('API is running...');
@@ -13,10 +15,12 @@ app.get(['/', '/index', '/index.html'], (req, res) => {
 
 app.use('/users', require('./routes/user.routes.js'))
 app.use('/posts', require('./routes/post.routes.js'))
+app.use('/auth', require('./routes/auth.routes.js'))
 
-app.use((req, res, next) => {
-    res.status(404).json({ message: `Route ${req.originalUrl} not found`})
-    next(app.use(errorHandler))
+app.use((req, res) => {
+  res.status(404).json({ message: `Route ${req.originalUrl} not found` })
 })
+
+app.use(errorHandler)
 
 module.exports = app
