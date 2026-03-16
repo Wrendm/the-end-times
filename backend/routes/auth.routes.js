aa33aefedd3f6da87d14e7e82f67ea0c29e1b41d
Wren Miles <wrendmiles@gmail.com>
2026-03-15 17:17:41 -0700

    feat(auth): create auth router

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -5,7 +5,12 @@ const validate = require('../validators/validate')
 const { createUserSchema } = require('../validators/user.validator')
 const { loginSchema } = require('../validators/login.validator')
 
+router.get('/me', authController.getCurrentUser)
+router.get('/refresh', authController.refreshUser)
+
 router.post('/register', validate(createUserSchema), authController.registerUser)
 router.post('/login', validate(loginSchema), authController.loginUser)
+router.post('/logout', authController.logoutUser)
+
 
 module.exports = router
