b7b9f35f80e6e9a5ed72065233cf32498380ed6d
Wren Miles <wrendmiles@gmail.com>
2026-03-15 17:20:24 -0700

    feat(auth): apply verifyJWT middleware to sensitive Post and User routes

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -4,25 +4,28 @@ const router = express.Router()
 const postsController = require('../controllers/posts.controller') 
 const validate = require('../validators/validate')
 const { createPostSchema, updatePostSchema, postIdParamSchema } = require('../validators/post.validator')
+const verifyJWT = require('../middleware/verifyJWT')
 
 
 router.route('/')
   .get(postsController.getAllPosts)
-  .post(validate(createPostSchema), postsController.createNewPost);
+  .post(verifyJWT, validate(createPostSchema), postsController.createNewPost);
 
 
 router.route('/:id')
   .get(validate(postIdParamSchema, 'params'), postsController.getSinglePost)
   .put(
+    verifyJWT,
     validate(postIdParamSchema, 'params'),
     validate(updatePostSchema),
     postsController.updatePost
   )
   .patch(
+    verifyJWT,
     validate(postIdParamSchema, 'params'),
     validate(updatePostSchema),
     postsController.updatePostPartial
   )
-  .delete(validate(postIdParamSchema, 'params'), postsController.deletePost);
+  .delete(verifyJWT, validate(postIdParamSchema, 'params'), postsController.deletePost);
 
 module.exports = router
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,24 +4,26 @@ const router = express.Router()
 const usersController = require('../controllers/users.controller')
 const validate = require('../validators/validate')
 const { createUserSchema, updateUserSchema, userIdParamSchema } = require('../validators/user.validator')
+const verifyJWT = require('../middleware/verifyJWT')
 
 router.route('/')
   .get(usersController.getAllUsers)
   .post(validate(createUserSchema), usersController.createNewUser);
 
-
 router.route('/:id')
   .get(validate(userIdParamSchema, 'params'), usersController.getSingleUser)
   .put(
+    verifyJWT,
     validate(userIdParamSchema, 'params'),
     validate(updateUserSchema),
     usersController.updateUser
   )
   .patch(
+    verifyJWT,
     validate(userIdParamSchema, 'params'),
     validate(updateUserSchema),
     usersController.updateUserPartial
   )
-  .delete(validate(userIdParamSchema, 'params'), usersController.deleteUser);
+  .delete(verifyJWT, validate(userIdParamSchema, 'params'), usersController.deleteUser);
 
 module.exports = router
