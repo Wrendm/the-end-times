87c5a362c8a579cd70e3f4fe0c2f2f72d8f563f9
Wren Miles <wrendmiles@gmail.com>
2026-03-16 17:28:43 -0700

    fix(auth): properly attach user.id and roles from access token

--- backend/middleware/verifyJWT.js
+++ backend/middleware/verifyJWT.js
@@ -1,29 +1,26 @@
 const jwt = require('jsonwebtoken')
-const asyncHandler = require('express-async-handler')
 
-const verifyJWT = asyncHandler(async (req, res, next) => {
+const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization
+    console.log('Auth header:', authHeader)
+    console.log('JWT decoded:', req.user);
+    console.log('Roles:', req.roles);
 
     if (!authHeader?.startsWith('Bearer')) {
         res.status(401)
-        throw new Error('Unauthorized')
+        throw new Error('Unauthorized: Auth header does not start with bearer')
     }
 
     const token = authHeader.split(' ')[1]
 
-    jwt.verify(
-        token,
-        process.env.ACCESS_TOKEN_SECRET,
-        async (err, decoded) => {
-            if (err) {
-                res.status(403)
-                throw new Error('Forbidden')
-            }
-            req.user = decoded.username
-            req.roles = decoded.roles
-            next()
-        }
-    )
-})
+    try {
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        req.user = { id: decoded.id, username: decoded.username }
+        req.roles = decoded.roles
+        next()
+    } catch (err) {
+        return res.sendStatus(403)
+    }
+}
 
 module.exports = verifyJWT
