8384788ec4253301a04c59db26f6a910253b7f38
Wren Miles <wrendmiles@gmail.com>
2026-03-19 17:01:10 -0700

    fix(auth): require(/models/User) for checking ownership

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../models/User')
 const usersController = require('../controllers/users.controller')
 const validate = require('../validators/validate')
 const { createUserSchema, updateUserSchema, userIdParamSchema } = require('../validators/user.validator')
 const verifyJWT = require('../middleware/verifyJWT')
+const checkOwnership = require('../middleware/checkOwnership')
 
 router.route('/')
   .get(usersController.getAllUsers)
