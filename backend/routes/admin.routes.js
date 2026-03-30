959513b92d7a9fb996d7ac432028d648d8be4d1b
Wren Miles <wrendmiles@gmail.com>
2026-03-29 17:15:38 -0700

    feat(categories): allow admins to update/delete categories

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const postsController = require('../controllers/posts.controller')
 const usersController = require('../controllers/users.controller')
+const categoriesController = require('../controllers/categories.controller')
 
 const verifyJWT = require('../middleware/verifyJWT')
 const verifyRoles = require('../middleware/verifyRoles')
@@ -10,18 +11,48 @@ const verifyRoles = require('../middleware/verifyRoles')
 const validate = require('../validators/validate')
 const { postQuerySchema } = require('../validators/post.validator')
 const { updateRolesSchema, userIdParamSchema } = require('../validators/user.validator')
+const { updateCategoryPutSchema, updateCategoryPatchSchema, categoryIdParamSchema } = require('../validators/category.validator')
 
 router.use(verifyJWT);
 router.use(verifyRoles('Admin'));
 
-router.get('/posts', validate(postQuerySchema, 'query'), postsController.getAllPostsAdmin);
-router.get('/users', usersController.getAllUsersAdmin);
+//posts
+router.route('/posts')
+    .get(validate(postQuerySchema, 'query'), postsController.getAllPostsAdmin)
+
+//users
+router.route('/users')
+    .get(usersController.getAllUsersAdmin)
 
 router.route('/users/:id/roles')
     .patch(
         validate(userIdParamSchema, 'params'),
         validate(updateRolesSchema, 'body'),
         usersController.updateRoles
+    )
+
+//categories
+router.route('/categories')
+    .get(categoriesController.getAllCategoriesAdmin)
+
+router.route('/categories/:id')
+    .get(
+        validate(categoryIdParamSchema, 'params'),
+        categoriesController.getSingleCategory
+    )
+    .put(
+        validate(categoryIdParamSchema, 'params'),
+        validate(updateCategoryPutSchema, 'body'),
+        categoriesController.updateCategory
+    )
+    .patch(
+        validate(categoryIdParamSchema, 'params'),
+        validate(updateCategoryPatchSchema, 'body'),
+        categoriesController.updateCategoryPartial
+    )
+    .delete(
+        validate(categoryIdParamSchema, 'params'),
+        categoriesController.deleteCategory
     )
 
 module.exports = router
