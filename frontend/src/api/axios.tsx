bfbb057a5956db1a82fdcad6f5fe1b04827ea6b5
Wren Miles <wrendmiles@gmail.com>
2026-04-13 18:30:48 -0700

    fix: attach JWT to axios requests via request interceptor

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -1,11 +1,26 @@
 import axios from "axios";
 import { setAccessToken } from "./tokenStorage";
+import { getAccessToken } from "./tokenStorage";
 import { redirectToLogin } from "../utils/redirect";
 
 const api = axios.create({
   baseURL: "/api",
   withCredentials: true
 });
+
+
+api.interceptors.request.use(
+  (config) => {
+    const token = getAccessToken();
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 api.interceptors.response.use(
   (res) => res,
